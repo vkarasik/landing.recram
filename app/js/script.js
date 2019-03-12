b41325d49aed32e8a3642e14c1da1a2c43c26e8b
Vadim Karasik <vadkarasik@gmail.com>
2019-03-12 20:47:38 +0300

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,38 @@
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var $linkHref = $(this).attr("href");
+        var $ancorName = $('a[name="' + $linkHref.slice(1) + '"]');
+        var ancorPosition = $ancorName.offset();
+        $('body, html').animate({
+            scrollTop: ancorPosition.top
+        }, 500);
+    })
+
+    $('.openform').on('click', function(){
+        $('.overlay').fadeIn()
+    });
+
+    $('.close').on('click', function(){
+        $('.overlay').fadeOut()
+    });
+
+    $('.form').submit(function(e){
+        e.preventDefault();// Отмена перезагрузки страницы при submit
+        var $form = $(this);
+        $.ajax({
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            success: function(response){
+                $('form .callback').css({background: '#4caf50'}).text('Заявка принята!');
+                setTimeout(function(){$('.overlay').fadeOut()}, 300);
+            },
+            error: function(response){
+                alert('Произошла ошибка! Пожалуйста повторите отправку!');  
+            }
+        });
+    });
+});
+
+
+// Я зрабіў усё што змог, хто зможа, хай зробіць лепш!
---
+++ app/source/links.md
@@ -0,0 +1,7 @@
+http://constanta-group.com/
+https://www.reckramlimited.com/
+
+colors — https://flatuicolors.com/palette/ca
+
+https://www.flaticon.com/packs/technology-collection
+https://www.flaticon.com/packs/electronic-device-14
